5a9dd5f51cd73cb58efc25ecce8e2f0bad75a045
ADITYA SENAPATI R <myselfaddy25@gmail.com>
2026-04-26 23:11:13 +0530

    Update groq_helper.py

--- groq_helper.py
+++ groq_helper.py
@@ -1,16 +1,21 @@
 from groq import Groq
+from dotenv import load_dotenv
 import os
 
 # =========================================================
 # VEILGuard - Groq AI Engine
 # =========================================================
 
-from dotenv import load_dotenv
-import os
-
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
+    raise ValueError("GROQ_API_KEY not found")
+
+client = Groq(
+    api_key=GROQ_API_KEY
+)
 
 # =========================================================
 # AI Alert Explanation
